5e932efee3bff5a1aa2cd8615dcf3e21d213e4e6
maemreyo <ngonhuthanhtrung1409@gmail.com>
2022-11-03 15:06:06 +0700

    fix: Fix fmt and clippy error

--- src/git/models/recent_active_repos.rs
+++ src/git/models/recent_active_repos.rs
@@ -1,37 +1,85 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Default, Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+#[derive(
+    Default,
+    Debug,
+    Clone,
+    PartialEq,
+    Eq,
+    Serialize,
+    Deserialize
+)]
 #[serde(rename_all = "camelCase")]
 pub struct Root {
     pub data: Data,
 }
 
-#[derive(Default, Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+#[derive(
+    Default,
+    Debug,
+    Clone,
+    PartialEq,
+    Eq,
+    Serialize,
+    Deserialize
+)]
 #[serde(rename_all = "camelCase")]
 pub struct Data {
     pub repository_owner: RepositoryOwner,
 }
 
-#[derive(Default, Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+#[derive(
+    Default,
+    Debug,
+    Clone,
+    PartialEq,
+    Eq,
+    Serialize,
+    Deserialize
+)]
 #[serde(rename_all = "camelCase")]
 pub struct RepositoryOwner {
     pub repositories: Repositories,
     pub avatar_url: String,
 }
 
-#[derive(Default, Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+#[derive(
+    Default,
+    Debug,
+    Clone,
+    PartialEq,
+    Eq,
+    Serialize,
+    Deserialize
+)]
 #[serde(rename_all = "camelCase")]
 pub struct Repositories {
     pub edges: Vec<Edge>,
 }
 
-#[derive(Default, Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+#[derive(
+    Default,
+    Debug,
+    Clone,
+    PartialEq,
+    Eq,
+    Serialize,
+    Deserialize
+)]
 #[serde(rename_all = "camelCase")]
 pub struct Edge {
     pub node: Node,
 }
 
-#[derive(Default, Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+#[derive(
+    Default,
+    Debug,
+    Clone,
+    PartialEq,
+    Eq,
+    Serialize,
+    Deserialize
+)]
 #[serde(rename_all = "camelCase")]
 pub struct Node {
     pub name: String,
